84cba571441f86ff78cc649f38531242458db484
AnjaliPrembhare <anjali.prembhare2001@gmail.com>
2024-08-25 12:57:34 +0530

    Update LoginPage.jsx

--- src/shared/components/LoginPage.jsx
+++ src/shared/components/LoginPage.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'boxicons/css/boxicons.min.css';
-import '../../assets/styles/login.css';
+// import '../../assets/styles/login.css';
 import { useNavigate } from "react-router-dom";
-import { Form, Button, Container, Row, Col, InputGroup } from 'react-bootstrap';
+// import { Form, Button, Container, Row, Col, InputGroup } from 'react-bootstrap';
 
 const LoginPage = () => {
 
