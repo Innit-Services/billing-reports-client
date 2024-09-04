bd1feee6c985232d5fc43e1aed71858741634dee
AnjaliPrembhare <anjali.prembhare2001@gmail.com>
2024-09-04 19:15:06 +0530

    added layout

--- src/shared/components/Layout.jsx
+++ src/shared/components/Layout.jsx
@@ -1,29 +1,40 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
-import Searchbar from "./SearchBar";
-import '../../assets/styles/flex.css';
-import '../../assets/styles/utils.css';
+import "../../assets/styles/flex.css";
+import "../../assets/styles/utils.css";
 
-function Layout(){
-    return(
-        <> 
-            <div className="d-flex column height-100-per">
-                <header className="flex-basis-10">
-                    <Navbar></Navbar>
-                    <Searchbar></Searchbar>
-                </header>
-                <div className="d-flex flex-basis-90" style={{Border:'2px solid'}}>
-                    <div className="flex-basis-10">
-                        <Sidebar></Sidebar>
-                    </div>
-                    <main className="flex-basis-90">
-                        <Outlet></Outlet>
-                    </main>
-                </div>
-            </div>
-        </>
-    )
+function Layout() {
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsExpanded((isExpanded) => !isExpanded);
+  };
+
+  return (
+    <div className="flex w-screen h-screen">
+      <div
+        className={`${
+            isExpanded ? "w-[13vw]" : "w-[4vw]"
+        } transition-all duration-300`}
+      >
+        <Sidebar isExpanded={isExpanded} toggleSidebar={toggleSidebar} />
+      </div>
+      <div
+        className={`h-screen ${
+            isExpanded ? "w-[87vw]" : "w-[96vw]"
+        } transition-all duration-300`}
+      >
+        <header className="h-[8vh] bg-white w-full border-b-0 border-gray-400 shadow-lg">
+          <Navbar />
+        </header>
+        <div className="h-[92vh] bg-gray-50">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Layout;
