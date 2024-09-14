05bf146835d33db1dad29d9d204dba12387ec3d7
AnjaliPrembhare <anjali.prembhare2001@gmail.com>
2024-09-14 16:15:18 +0530

    added navbar

--- src/shared/components/Navbar.jsx
+++ src/shared/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'boxicons/css/boxicons.min.css';
 import '../../assets/styles/flex.css';
-import '../../assets/styles/navbar.css';
+
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-light">
+    <nav className="navbar">
       <div className="container-fluid d-flex justify-content-between align-items-center">
         <div className="leftnav"></div>
         <div className="d-flex align-items-center">
-          <i className="bx bx-user-circle fs-3 me-2"></i>
-          <span className="fw-bold">Hello, User</span>
+        <i class='bx bxs-cog fs-3 me-3'></i>
+          <i className="bx bx-user-circle fs-3 me-2 pt-2"></i>
         </div>
       </div>
     </nav>
