185d4a9bff86fe38b973da4ea664ba5c5ba3ec01
AnjaliPrembhare <anjali.prembhare2001@gmail.com>
2024-09-17 10:44:01 +0530

    added navbar

--- src/shared/components/Navbar.jsx
+++ src/shared/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'boxicons/css/boxicons.min.css';
 import '../../assets/styles/flex.css';
+import Tooltip from '@mui/material/Tooltip';
 
 
 const Navbar = () => {
@@ -10,8 +11,12 @@ const Navbar = () => {
       <div className="container-fluid d-flex justify-content-between align-items-center">
         <div className="leftnav"></div>
         <div className="d-flex align-items-center">
+       
         <i class='bx bxs-cog fs-3 me-3'></i>
-          <i className="bx bx-user-circle fs-3 me-2 pt-2"></i>
+   
+        <Tooltip title="Profile" arrow>
+          <i className="bx bx-user-circle fs-3 me-2"></i>
+          </Tooltip>
         </div>
       </div>
     </nav>
