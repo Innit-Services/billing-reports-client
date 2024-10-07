3b0b2fb0ca1045fed69d0481cd1c7f29ca8b4cfb
AnjaliPrembhare <anjali.prembhare2001@gmail.com>
2024-10-07 19:47:03 +0530

    added package lock json file

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,16 @@ import NotFound from "./shared/components/NotFound";
 import AddClient from "./module/client/components/AddClient";
 import ViewClient from "./module/client/components/ViewClient";
 import LoginPage from "./shared/components/LoginPage";
-// import RegisterForms from "./shared/components/RegisterForms";
 import SignUpPage from "./shared/components/SignUpPage";
 import UpdateClient from "./module/client/components/UpdateClient";
 import AddEmployeeStatus from "./module/employee/components/AddEmployeeStatus";
 import ViewDepartment from "./module/department/components/ViewDepartment";
+import ViewProfile from "./module/employee/components/ViewProfile";
+import ViewPosition from "./module/position/components/ViewPosition";
+import { ThemeProvider } from "./apis/ThemeContext.jsx";
+import ViewEmployeeStatus from "./module/employee/components/ViewEmployeeStatus.jsx";
+import ViewWages from "./module/employee/components/ViewWages.jsx";
+import ViewEmployeePosition from "./module/employee/components/ViewEmployeePosition.jsx";
 
 
 const routes = [
@@ -24,17 +29,26 @@ const routes = [
       { index: true, element: <>Default Landing Page</> },
       { path: "/addemployee", element: <AddEmployee /> },
       { path: "viewemployee", element: <ViewEmployee /> },
+      { path: "viewposition", element: <ViewPosition/>},
+      { path: "/viewprofile/:id", element: <ViewProfile/>},
       { path: "/addemployeestatus", element:<AddEmployeeStatus/>},
+      { path: "/viewemployeestatus/:id", element:<ViewEmployeeStatus/>},
+      { path: "/viewemployeepositions/:id", element:<ViewEmployeePosition/>},
+      { path: "/viewwages", element:<ViewWages/>},
       { path:"viewdepartment", element: <ViewDepartment/>},
       { path: "addclient", element:<AddClient/>},
       { path: "viewclient", element: <ViewClient /> },
-      { path: "updateclient", element: <UpdateClient/>},
+      { path: "updateclient", element: <UpdateClient/>},   
       { path: "*", element: <NotFound /> },
+  
+     
+     
     ],
   },
   {
     path: "/signin",
     element: <LoginPage />
+  
   },
   {
     path: "/signup",
@@ -49,8 +63,11 @@ function App() {
     <>
     {/* declarative route config will be moved to more programatic style, to achieve lazy loading/Code splitting */}
 
-    
+    <ThemeProvider>
       <RouterProvider router={router}></RouterProvider>
+      
+      </ThemeProvider>
+      
       </>);
 }
 export default App;
